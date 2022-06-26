59ef2ea760ab98a53c36550e81416aae991e1df2
liuzhuan <liuz_geek@163.com>
2022-06-26 09:54:13 +0800

    feat: complete first draft

--- shopping-calc/src/App.vue
+++ shopping-calc/src/App.vue
@@ -1,21 +1,117 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, watch, onMounted } from 'vue'
+import ShoppingItem from './components/ShoppingItem.vue'
+
+const cachedKey = 'shopping-calc.app'
+
+const list = ref([])
+
+onMounted(() => {
+  list.value = load()
+})
+
+const sum = computed(() => {
+  return list.value
+    .map(({ price, quantity }) => price * quantity)
+    .reduce((a, b) => a + b, 0)
+})
+
+function onDel(idx) {
+  list.value.splice(idx, 1)
+}
+
+function onClearAll() {
+  list.value = []
+}
+
+function onAdd() {
+  const defaultLabel = '默认商品' + (list.value.length + 1)
+  list.value.push({ label: defaultLabel, price: 0, quantity: 1 })
+}
+
+function store(val) {
+  localStorage.setItem(cachedKey, JSON.stringify(val))
+}
+
+function load() {
+  try {
+    const raw = localStorage.getItem(cachedKey)
+    const parsed = JSON.parse(raw)
+    return parsed || []
+  } catch (e) {
+    return []
+  }
+}
+
+watch(
+  list,
+  (newVal) => {
+    store(newVal)
+  },
+  {
+    deep: true,
+  }
+)
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main class="calc">
+    <h1>购物计算器</h1>
+
+    <div class="action">
+      <button @click="onAdd">添加</button>
+      <button @click="onClearAll" class="ml1">清空所有</button>
+    </div>
+    <div class="list" v-if="list.length <= 0">没有数据</div>
+    <ol class="list" v-else>
+      <li v-for="(item, index) of list">
+        <ShoppingItem
+          v-model:label="item.label"
+          v-model:price.number="item.price"
+          v-model:quantity.number="item.quantity"
+          @del="onDel(index)"
+        />
+      </li>
+    </ol>
+
+    <div class="sum">总额：{{ sum }} 元，共计 {{ list.length }} 件商品</div>
+  </main>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.calc {
+  padding: 1em;
+}
+
+.sum {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 1em;
+  background: steelblue;
+  color: #fff;
+}
+
+.list {
+  padding: 1em;
+  padding-bottom: 5em;
+}
+
+.ml1 {
+  margin-left: 1em;
+}
+
+.ml2 {
+  margin-left: 0.5em;
+}
+
+li {
+  margin-bottom: 0.5em;
 }
 </style>
---
+++ shopping-calc/src/components/ShoppingItem.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps(['label', 'price', 'quantity'])
+defineEmits(['update:label', 'update:price', 'update:quantity', 'del'])
+</script>
+
+<template>
+  <div>
+    <input
+      type="text"
+      placeholder="名称"
+      :value="label"
+      @input="$emit('update:label', $event.target.value)"
+    />
+    <input
+      type="number"
+      min="0"
+      max="9999"
+      class="ml2"
+      placeholder="单价"
+      :value="price"
+      @input="$emit('update:price', $event.target.value)"
+    />
+    <input
+      type="number"
+      min="0"
+      max="999"
+      class="ml2"
+      placeholder="数量"
+      :value="quantity"
+      @input="$emit('update:quantity', $event.target.value)"
+    />
+    <button class="ml2" @click="$emit('del')">删除</button>
+  </div>
+</template>
